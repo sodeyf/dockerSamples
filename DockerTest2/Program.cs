4bba7acacd8485a3e7e8c7e10f22e3fb2effe521
Sodeyf Qeidarlo <sodeyf@gmail.com>
2024-09-05 12:02:29 +0330

    Kestrel added

--- DockerTest2/Program.cs
+++ DockerTest2/Program.cs
@@ -1,10 +1,22 @@
 var builder = WebApplication.CreateBuilder(args);
 
+// No need to specify URLs here; it will read from configuration.
+builder.WebHost.ConfigureKestrel(options =>
+{
+    var url = builder.Configuration.GetSection("Kestrel");
+    options.Configure(url);
+});
+
+// Explicitly configure Kestrel to use the settings from configuration, including URLs.
+//builder.WebHost.UseKestrel();
+
+builder.WebHost.UseUrls("http://*:8010");
+
 // Add services to the container.
 
 builder.Services.AddControllers();
 
-builder.WebHost.UseUrls("http://*:8010");
+
 
 var app = builder.Build();
 
